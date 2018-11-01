a53d4db08ba17c492cf07bdc6230659624519535
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-11-01 13:45:01 -0600

    Compass is :b:roke

--- musicbot/lib/cycleDb.js
+++ musicbot/lib/cycleDb.js
@@ -1,24 +1,23 @@
 const database = _connections.get('database').db;
 const discord = _connections.get('discord').client;
-let collections = database.listCollections();
-collections = collections.toArray();
 
 let document = { _version: 1, _plugin: "musicbot", queue: [] };
 let query = { _plugin: "musicbot" };
 
-collections.then(array => {
-    for (item in array) {
-        if (item[1].name.startsWith("guild_")) {
-            let cursor = item.find(query).toArray();
+
+database.collections().then(items => {
+    for (i in items) {
+        if (items[i].collectionName.startsWith("guild_")) {
+            let cursor = items[i].find(query).toArray();
             cursor.length > 1
-            ? _logger.warn({ prefix: "Database", message: `${item.name} contains more than 1 musicbot object. May cause issues.`})
+            ? _logger.warn({ prefix: "Database", message: `${items[i].name} contains more than 1 musicbot object. May cause issues.`})
             : cursor.length < 1
-            ? item.insertOne(document, { forceServerObjectId: true })
+            ? items[i].insertOne(document, { forceServerObjectId: true })
             : () => {
-                item.findOneAndReplace({ _version: { $ne: document._version}}, document, original => {
-                    item.updateOne({ _version: document._version }, document.queue = original.queue);
+                items[i].findOneAndReplace({ _version: { $ne: document._version}}, document, original => {
+                    items[i].updateOne({ _version: document._version }, document.queue = original.queue);
                 });
             }
         }
     }
-})
+});
